44a8397606c923072cfa8ad0871f4c149153a42f
JonatanAlexisYanezRivas <jonatanalexis@live.com>
2022-01-03 13:08:56 -0600

    first

---
+++ reset.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function(){
+    if(!Notification){
+        alert("Las notificaciones no se soportan en tu navegador, bajate uno nuevo");
+        return;
+    }
+
+    if(Notification.permission !== "granted"){
+        Notification.requestPermission();
+    }
+});
+
+
+    if(Notification.permission !== "granted"){
+        Notification.requestPermission();
+    }else{
+        var notificacion = new Notification("No se pudo resetear el ID",
+        {
+            icon: "tap.png",
+            body: "para hacer reset no debe haber registros en la base de datos"
+        }
+        );
+
+        notificacion.onclick = function(){
+            window.open("http://localhost/proyectoFinal/principal.php");
+        }
+
+    }
